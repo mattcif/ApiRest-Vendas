803b2f91c085909e2e79f842c94f674e8273756f
dr-if <matthewoaklucky@gmail.com>
2023-09-20 12:17:00 -0300

    update, delete, search by name

--- src/main/java/org/example/VendasApplication.java
+++ src/main/java/org/example/VendasApplication.java
@@ -17,11 +17,37 @@ public class VendasApplication {
     @Bean
     public CommandLineRunner init(@Autowired Clientes clientes){
         return args -> {
+            System.out.println("Salvando clientes");
             clientes.salvar(new Cliente("Matheus"));
             clientes.salvar(new Cliente("Ari"));
 
             List<Cliente> todosClientes = clientes.obterTodos();
             todosClientes.forEach(System.out::println);
+
+            System.out.println("Atualizando clientes");
+            todosClientes.forEach(c -> {
+                c.setNome(c.getNome() + " atualizado.");
+                clientes.atualizar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Buscando cliente (\"Ar\")");
+            clientes.buscarPorNome("Ar").forEach(System.out::println);
+
+//            System.out.println("deletando clientes");
+//            clientes.obterTodos().forEach(c -> {
+//                clientes.deletar(c);
+//            });
+            todosClientes = clientes.obterTodos();
+            if(todosClientes.isEmpty()){
+                System.out.println("Nenhum cliente encontrado.");
+            } else{
+                todosClientes.forEach(System.out::println);
+
+            }
+
 
         };
     }
