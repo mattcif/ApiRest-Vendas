f862c6c7cf935328eeccb08b0a6f3d02a8804c67
dr-if <matthewoaklucky@gmail.com>
2023-09-20 20:41:19 -0300

    jpa em cliente

--- src/main/java/org/example/domain/entity/Cliente.java
+++ src/main/java/org/example/domain/entity/Cliente.java
@@ -1,5 +1,9 @@
 package org.example.domain.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table (name = "cliente")
 public class Cliente {
     public Cliente(){}
 
@@ -11,8 +15,11 @@ public class Cliente {
         this.id = id;
         this.nome = nome;
     }
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column (name = "id")
     private Integer id;
+    @Column (name = "nome", length = 100)
     private String nome;
 
     public Integer getId() {
@@ -39,3 +46,4 @@ public class Cliente {
                 '}';
     }
 }
+
