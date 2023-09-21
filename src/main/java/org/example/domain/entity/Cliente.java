c1f17e356fee9315fd9e46a6e6af8356290edab7
dr-if <matthewoaklucky@gmail.com>
2023-09-21 17:54:55 -0300

    método get find

--- src/main/java/org/example/domain/entity/Cliente.java
+++ src/main/java/org/example/domain/entity/Cliente.java
@@ -23,6 +23,8 @@ public class Cliente {
     private Integer id;
     @Column (name = "nome", length = 100)
     private String nome;
+    @Column(name = "cpf", length = 11)
+    private String cpf;
     @JsonIgnore
     @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY) // lazy tem melhor desempenho
     private Set<Pedido> pedidos;
@@ -49,6 +51,14 @@ public class Cliente {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 
     @Override
--- src/main/java/org/example/rest/controller/ClienteController.java
+++ src/main/java/org/example/rest/controller/ClienteController.java
@@ -2,10 +2,13 @@ package org.example.rest.controller;
 
 import org.example.domain.entity.Cliente;
 import org.example.domain.repository.Clientes;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -58,5 +61,15 @@ public class ClienteController {
                     clientes.save(cliente);
                     return ResponseEntity.noContent().build();
                 }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+    @GetMapping("/api/clientes")
+    public ResponseEntity find(Cliente filtro){
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);//containing retorn todos que contem a busca
+        Example example = Example.of(filtro, matcher);
+        List<Cliente> lista =  clientes.findAll(example);
+        return ResponseEntity.ok(lista);
     }
 }
