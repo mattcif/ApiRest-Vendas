5d0fc531540b05865a7c380b0999ebdab54e8e3c
dr-if <matthewoaklucky@gmail.com>
2023-09-21 10:13:37 -0300

    query methods

--- src/main/java/org/example/VendasApplication.java
+++ src/main/java/org/example/VendasApplication.java
@@ -19,32 +19,8 @@ public class VendasApplication {
             clientes.save(new Cliente("Matheus"));
             clientes.save(new Cliente("Ari"));
 
-            List<Cliente> todosClientes = clientes.findAll();
-            todosClientes.forEach(System.out::println);
-
-            System.out.println("Atualizando clientes");
-            todosClientes.forEach(c -> {
-                c.setNome(c.getNome() + " atualizado.");
-                clientes.save(c);
-            });
-
-            todosClientes = clientes.findAll();
-            todosClientes.forEach(System.out::println);
-
-            System.out.println("Buscando cliente (\"Ar\")");
-            clientes.findByNomeLike("Ar").forEach(System.out::println);
-
-            System.out.println("deletando clientes");
-            clientes.findAll().forEach(c -> {
-                clientes.delete(c);
-            });
-            todosClientes = clientes.findAll();
-            if(todosClientes.isEmpty()){
-                System.out.println("Nenhum cliente encontrado.");
-            } else{
-                todosClientes.forEach(System.out::println);
-
-            }
+            boolean existe = clientes.existsByNome("Ar");
+            System.out.println("existe cliente com o nome 'Ari' " + existe );
 
 
         };
