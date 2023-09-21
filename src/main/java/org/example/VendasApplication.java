b704bb0b207a977b3e0b909392e0f21acb069813
dr-if <matthewoaklucky@gmail.com>
2023-09-21 14:55:12 -0300

    criando controller cliente

--- src/main/java/org/example/VendasApplication.java
+++ src/main/java/org/example/VendasApplication.java
@@ -1,50 +1,10 @@
 package org.example;
 
-import org.example.domain.entity.Cliente;
-import org.example.domain.entity.Pedido;
-import org.example.domain.repository.Clientes;
-import org.example.domain.repository.Pedidos;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class VendasApplication {
-    @Bean
-    public CommandLineRunner init(
-            @Autowired Clientes clientes,
-            @Autowired Pedidos pedidos){
-        return args -> {
-            System.out.println("Salvando clientes");
-            Cliente ana = new Cliente("Ana");
-            clientes.save(ana);
-            clientes.save(new Cliente("Matheus"));
-            clientes.save(new Cliente("Ari"));
-
-            Pedido p = new Pedido();
-            p.setCliente(ana);
-            p.setDataPedido(LocalDate.now());
-            p.setTotal(BigDecimal.valueOf(100));
-
-            pedidos.save(p);
-
-//            Cliente clienteFetchPedidos = clientes.findClienteFetchPedidos(ana.getId());
-//            System.out.println(clienteFetchPedidos);
-//            System.out.println(clienteFetchPedidos.getPedidos());
-            System.out.println("Listando pedidos cliente");
-            pedidos.findByCliente(ana).forEach(System.out::println);
-
-            boolean existe = clientes.existsByNome("Ar");
-            System.out.println("existe cliente com o nome 'Ari' " + existe );
-
-
-        };
-    }
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
---
+++ src/main/java/org/example/rest/controller/ClienteController.java
@@ -0,0 +1,17 @@
+package org.example.rest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api/clientes")
+public class ClienteController {
+    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloCliente(@PathVariable("nome") String nomeCLiente) {
+        return String.format("Hello %s ", nomeCLiente);
+    }
+}
