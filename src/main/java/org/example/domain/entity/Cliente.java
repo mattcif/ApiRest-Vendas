b70b41224213df1c24fc4d4fb7682a6b2b1a589f
dr-if <matthewoaklucky@gmail.com>
2023-09-22 18:54:15 -0300

    aplicando plugin lombok

--- src/main/java/org/example/domain/entity/Cliente.java
+++ src/main/java/org/example/domain/entity/Cliente.java
@@ -1,17 +1,17 @@
 package org.example.domain.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 import javax.persistence.*;
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table (name = "cliente")
 public class Cliente {
-    public Cliente(){}
-
-    public Cliente(String nome) {
-        this.nome = nome;
-    }
 
     public Cliente(Integer id, String nome) {
         this.id = id;
@@ -29,44 +29,6 @@ public class Cliente {
     @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY) // lazy tem melhor desempenho
     private Set<Pedido> pedidos;
 
-    public Set<Pedido> getPedidos() {
-        return pedidos;
-    }
 
-    public void setPedidos(Set<Pedido> pedidos) {
-        this.pedidos = pedidos;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    @Override
-    public String toString() {
-        return "Cliente{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                '}';
-    }
 }
 
