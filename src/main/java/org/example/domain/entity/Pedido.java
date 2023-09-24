846c83f9fdaa5e1bbea2d9f47a37a3b11cdc01f2
dr-if <matthewoaklucky@gmail.com>
2023-09-24 17:51:21 -0300

    enum StatusPedido

--- src/main/java/org/example/domain/entity/Pedido.java
+++ src/main/java/org/example/domain/entity/Pedido.java
@@ -2,6 +2,7 @@ package org.example.domain.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.domain.enums.StatusPedido;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -23,6 +24,9 @@ public class Pedido {
     private LocalDate dataPedido;
     @Column(name = "total", precision = 20, scale = 2)
     private BigDecimal total;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private StatusPedido status;
     @OneToMany(mappedBy = "pedido")
     private List<ItemPedido> itens;
 
--- src/main/java/org/example/rest/dto/InformacoesPedidoDTO.java
+++ src/main/java/org/example/rest/dto/InformacoesPedidoDTO.java
@@ -18,5 +18,7 @@ public class InformacoesPedidoDTO {
     private String nomeCliente;
     private BigDecimal total;
     private String dataPedido;
+    private String status;
     private List<InformacaoItemPedidoDTO> items;
+
 }
