5459cf00341820f1f31f656cfa9a22fcbc9a9097
dr-if <matthewoaklucky@gmail.com>
2023-09-26 19:07:12 -0300

    Bean Validation Pedido, idcliente e total

--- src/main/java/org/example/rest/dto/PedidoDTO.java
+++ src/main/java/org/example/rest/dto/PedidoDTO.java
@@ -6,13 +6,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PedidoDTO {
+    @NotNull(message = " Informe o código do cliente.")
     private Integer cliente;
+    @NotNull(message = "Campo Total do pedido é obrigatório.")
     private BigDecimal total;
     private List<ItemPedidoDTO> items;
 }
