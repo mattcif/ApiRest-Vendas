cd56a115e6177b7d0e8304cdaba2f92f4aaa986e
dr-if <matthewoaklucky@gmail.com>
2023-09-27 15:19:20 -0300

    autenticacao SecurityConfig

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.example.config;
 import org.example.service.impl.UsuarioServiceimpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,14 +30,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/clientes/**")
-                .hasAnyRole("USER", "ADMIN")
-                .antMatchers("/api/pedidos/**")
-                .hasAnyRole("USER", "ADMIN")
-                .antMatchers("/api/produtos/**")
-                .hasRole("ADMIN")
+                    .antMatchers("/api/clientes/**")
+                        .hasAnyRole("USER", "ADMIN")
+                    .antMatchers("/api/pedidos/**")
+                        .hasAnyRole("USER", "ADMIN")
+                    .antMatchers("/api/produtos/**")
+                        .hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/usuarios/**")
+                    .permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .httpBasic();
+                    .httpBasic();
     }
 
 
